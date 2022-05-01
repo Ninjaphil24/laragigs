9d1501afa28481a953637fa1d008704f6c64f175
Ninjaphil24 <mphilippos@hotmail.com>
2022-05-01 09:36:08 +0300

    Routes part 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function(){
+    return response('<h1>Hello World</h1>');
+});
+
+Route::get('/posts/{id}',function($id){
+    return response('Post '.$id);
+})->where('id', '[0-9]+');
