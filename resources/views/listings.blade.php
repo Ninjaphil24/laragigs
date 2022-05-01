a8703f3ecea1cb71b55bd0b5b68fcea77c8ac508
Ninjaphil24 <mphilippos@hotmail.com>
2022-05-01 16:45:50 +0300

    Basic blade

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,33 @@
+{{-- Php syntax --}}
+{{-- <h1><?php echo $heading?></h1>
+<?php foreach($listings as $listing): ?>
+    <h2><?php echo $listing['title'];?></h2>
+    <p><?php echo $listing['description'];?></p>
+<?php endforeach;?> --}}
+
+{{-- Blade Syntax --}}
+<h1>{{$heading}}</h1>
+
+{{-- 
+    This is another way to call php stuff in blade
+    @php
+$test=1;
+@endphp
+{{$test}} --}}
+
+
+{{-- @if(count($listings)==0)
+<p>No listings found</p>
+@endif --}}
+
+{{-- unless is a variation of if else --}}
+@unless(count($listings)==0)
+
+@foreach($listings as $listing)
+    <h2>{{$listing['title']}}</h2>
+    <p>{{$listing['description']}}</p>
+@endforeach
+
+@else
+    <p>No listings found</p>
+@endunless
