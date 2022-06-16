8d31d4fcdaca6a7a8632c7062d8dc3e23dddf20c
unknown <philipmodino@gmail.com>
2022-06-16 10:22:56 +0200

    Registration complete

--- routes/web.php
+++ routes/web.php
@@ -60,3 +60,6 @@ Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 // Show Register/Create Form
  Route::get('/register',[UserController::class, 'create']);
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
