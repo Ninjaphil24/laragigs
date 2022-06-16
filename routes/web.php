d9eb304c748102b4f2084168412587d1be2fe982
unknown <philipmodino@gmail.com>
2022-06-16 09:28:43 +0200

    Register part 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -57,4 +58,5 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
- 
+// Show Register/Create Form
+ Route::get('/register',[UserController::class, 'create']);
